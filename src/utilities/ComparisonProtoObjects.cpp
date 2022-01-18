16202c13a1131109444c776e4c7cd87af457fa07
mykhailo.kupiianov <mykhailo.kupriianov@teamdev.com>
2022-01-18 17:15:51 +0200

    Comparison fixed

--- src/utilities/ComparisonProtoObjects.cpp
+++ src/utilities/ComparisonProtoObjects.cpp
@@ -11,21 +11,11 @@ bool operator==(const TaskId& first, const TaskId& second) {
     return first.value() == second.value();
 }
 
-bool operator==(const ::google::protobuf::RepeatedPtrField <std::string>& first,
-                const ::google::protobuf::RepeatedPtrField <std::string>& second) {
-    std::set<std::string> fi;
-    std::set<std::string> se;
-    for (const auto& label: first)
-        fi.insert(label);
-    for (const auto& label: second)
-        se.insert(label);
-    return fi == se;
-}
-
 bool operator==(const Task& first, const Task& second) {
     return first.title() == second.title() && first.priority() == second.priority() &&
            first.has_date() == second.has_date() && first.date().seconds() == second.date().seconds() &&
-           first.labels() == second.labels() && first.status() == second.status();
+           first.status() == second.status() && first.labels().size() == second.labels().size() &&
+           std::equal(first.labels().begin(), first.labels().end(), second.labels().begin());
 }
 
 bool operator==(const HierarchicalTask& first, const HierarchicalTask& second) {
