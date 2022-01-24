a2f541bc2625f27ff38074cc4b8efe004c24846c
mykhailo.kupiianov <mykhailo.kupriianov@teamdev.com>
2022-01-24 16:20:39 +0200

    Create array simplified

--- tests/utilities/SortingProtoObjectsTest.cpp
+++ tests/utilities/SortingProtoObjectsTest.cpp
@@ -12,10 +12,7 @@ TEST_F(SortingProtoObjectsTest, shouldSortById) {
     TaskWithId task_1{CreateTaskId(0), CreateTask("first")},
             task_2{CreateTaskId(2), CreateTask("second")},
             task_3{CreateTaskId(1), CreateTask("third")};
-    ManyTasksWithId tasks;
-    tasks.push_back(task_1);
-    tasks.push_back(task_2);
-    tasks.push_back(task_3);
+    ManyTasksWithId tasks{task_1, task_2, task_3};
 
     SortTasksWithId(tasks, TasksSortBy::ID);
     EXPECT_EQ(tasks[0], task_1);
@@ -27,10 +24,7 @@ TEST_F(SortingProtoObjectsTest, shouldSortByPriority) {
     TaskWithId task_1{CreateTaskId(0), CreateTask("", model::Task_Priority_LOW)},
             task_2{CreateTaskId(1), CreateTask("", model::Task_Priority_NONE)},
             task_3{CreateTaskId(2), CreateTask("", model::Task_Priority_HIGH)};
-    ManyTasksWithId tasks;
-    tasks.push_back(task_1);
-    tasks.push_back(task_2);
-    tasks.push_back(task_3);
+    ManyTasksWithId tasks{task_1, task_2, task_3};
 
     SortTasksWithId(tasks, TasksSortBy::PRIORITY);
     EXPECT_EQ(tasks[0], task_3);
@@ -42,10 +36,7 @@ TEST_F(SortingProtoObjectsTest, shouldSortByDate) {
     TaskWithId task_1{CreateTaskId(0), CreateTask("", model::Task_Priority_NONE, 200)},
             task_2{CreateTaskId(1), CreateTask("", model::Task_Priority_NONE, 100)},
             task_3{CreateTaskId(2), CreateTask("", model::Task_Priority_NONE, 300)};
-    ManyTasksWithId tasks;
-    tasks.push_back(task_2);
-    tasks.push_back(task_1);
-    tasks.push_back(task_3);
+    ManyTasksWithId tasks{task_1, task_2, task_3};
 
     SortTasksWithId(tasks, TasksSortBy::DATE);
     EXPECT_EQ(tasks[0], task_2);
