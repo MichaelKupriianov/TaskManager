dcee674ab3f57b9673795f6838415e99a2b08102
mykhailo.kupiianov <mykhailo.kupriianov@teamdev.com>
2022-01-27 18:21:49 +0200

    ui::GRPCEndPoint added

--- src/ui/controller/GRPCEndPoint.h
+++ src/ui/controller/GRPCEndPoint.h
@@ -1,14 +1,32 @@
-//
-// Created by Mykhailo Kupiianov on 27.01.2022.
-//
+#pragma once
 
-#ifndef TASKMANAGER_GRPCENDPOINT_H
-#define TASKMANAGER_GRPCENDPOINT_H
+#include <memory>
+#include <utility>
+#include <grpcpp/grpcpp.h>
+#include "ModelService.grpc.pb.h"
+#include "model/Model.h" //?
+#include "Controller.h"  //?
 
+namespace ui {
+class GRPCEndPoint : public model::Model {
+public:
+    explicit GRPCEndPoint(const std::shared_ptr<ModelService::StubInterface>& stub) :
+            stub_{stub} {}
 
-class GRPCEndPoint {
+    bool AddTask(const model::Task& task) override;
+    bool AddSubTask(const model::Task& task, const model::TaskId& id) override;
+    bool Edit(const model::TaskId& id, const model::Task& task) override;
+    bool Complete(const model::TaskId& id) override;
+    bool Delete(const model::TaskId& id) override;
 
+    model::ManyTasksWithId ShowByLabel(const std::string& label, const model::TasksSortBy&) override;
+    model::ManyTasksWithId ShowParents(const model::TasksSortBy&) override;
+    model::CompositeTask ShowTask(const model::TaskId& id, const model::TasksSortBy&) override;
+    model::ManyCompositeTasks ShowAll(const model::TasksSortBy&) override;
+
+    bool Save(const std::string& filename) override;
+    bool Load(const std::string& filename) override;
+private:
+    std::shared_ptr<ModelService::StubInterface> stub_;
 };
-
-
-#endif //TASKMANAGER_GRPCENDPOINT_H
+}
