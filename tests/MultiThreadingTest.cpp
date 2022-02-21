ce9a7d1824153c80b566efdd216166de39ae53c8
mykhailo.kupiianov <mykhailo.kupriianov@teamdev.com>
2022-02-21 15:06:15 +0200

    Commands counter added to tests

--- tests/MultiThreadingTest.cpp
+++ tests/MultiThreadingTest.cpp
@@ -7,6 +7,7 @@ public:
     void SetUp() override {
         model_ = std::make_shared<model::Model>(
                 std::make_shared<model::TaskManager>(std::make_shared<model::IdGenerator>()));
+        commands_counter_ = 0;
     }
 
     void ExecuteRandomCommand() {
@@ -18,26 +19,48 @@ public:
         switch (type) {
             case 0:
                 model_->AddTask(task);
+                commands_counter_++;
+                break;
             case 1:
                 model_->AddSubTask(task, id);
+                commands_counter_++;
+                break;
             case 2:
                 model_->Edit(id, task);
+                commands_counter_++;
+                break;
             case 3:
                 model_->Complete(id);
+                commands_counter_++;
+                break;
             case 4:
                 model_->Delete(id);
+                commands_counter_++;
+                break;
             case 5:
                 model_->ShowByLabel("", sort_by);
+                commands_counter_++;
+                break;
             case 6:
                 model_->ShowParents(sort_by);
+                commands_counter_++;
+                break;
             case 7:
                 model_->ShowTask(id, sort_by);
+                commands_counter_++;
+                break;
             case 8:
                 model_->ShowAll(sort_by);
+                commands_counter_++;
+                break;
             case 9:
                 model_->Save("multi_threading_test");
+                commands_counter_++;
+                break;
             case 10:
                 model_->Load("multi_threading_test");
+                commands_counter_++;
+                break;
         }
     }
 
@@ -47,18 +70,20 @@ public:
     }
 protected:
     std::shared_ptr<model::Model> model_;
+    std::atomic<int> commands_counter_;
 };
 
 TEST_F(MultiThreadingTest, shouldWorkWhenReceivesManyCommandsFromDifferentThreads) {
-    int number_of_thread = 16;
-    int number_of_commands = 1024;
+    int number_of_threads = 16;
+    int number_of_commands = 2048;
 
     std::vector<std::thread> threads;
-    for (int i = 0; i < number_of_thread; i++)
+    for (int i = 0; i < number_of_threads; i++)
         threads.emplace_back(&MultiThreadingTest::Run, this, number_of_commands);
 
     for (auto& thread: threads)
         thread.join();
 
     remove("multi_threading_test");
+    EXPECT_EQ(number_of_threads * number_of_commands, commands_counter_);
 }
