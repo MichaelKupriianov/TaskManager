9b2f0d62e3a1e690fd525cbdd06d588d30c7178a
mykhailo.kupiianov <mykhailo.kupriianov@teamdev.com>
2022-02-22 18:52:15 +0200

    Added documentation for ui::StateMachine

--- src/model/Model.h
+++ src/model/Model.h
@@ -6,15 +6,15 @@
 namespace model {
 
 /**
- * \brief Interface of core
- * \details Manages TaskManager and TaskPersister
+ * \brief Manager of core
+ * \details Manages \b TaskManager and \b TaskPersister
  * \author Michael Kupriianov
  */
 class Model {
 public:
     /**
      * Constructor that creates an instance of the model
-     * @param manager - pointer to task manager instance
+     * @param manager - pointer to \b TaskManager instance
      */
     explicit Model(const std::shared_ptr<model::TaskManager>& manager) :
             manager_{manager} {}
@@ -26,34 +26,34 @@ public:
     /**
      * Adds a new task
      * @param task - protobuf message containing the task to be added
-     * @return bool - is addition successful
+     * @return \b bool - is addition successful
      */
     virtual bool AddTask(const Task& task);
     /**
      * Adds a new subtask with exiting parent
      * @param task - protobuf message containing the subtask to be added
      * @param id - protobuf message containing the id of parent
-     * @return bool - is addition successful
+     * @return \b bool - is addition successful
      */
     virtual bool AddSubTask(const Task& task, const TaskId& id);
     /**
      * Edits existing task
      * @param id - protobuf message containing id of task to be edited
      * @param task - protobuf message containing new content of task
-     * @return bool - is edition successful
+     * @return \b bool - is edition successful
      */
     virtual bool Edit(const TaskId& id, const Task& task);
     /**
      * Completes existing task
      * @param id - protobuf message containing id of task to be completed
-     * @return bool - is completion successful
+     * @return \b bool - is completion successful
      * @warning All subtasks of this task will also be completed
      */
     virtual bool Complete(const TaskId& id);
     /**
      * Deletes existing task
      * @param id - protobuf message containing id of task to be deleted
-     * @return bool - is deletion successful
+     * @return \b bool - is deletion successful
      * @warning All subtasks of this task will also be deleted
      */
     virtual bool Delete(const TaskId& id);
@@ -62,40 +62,40 @@ public:
      * Shows all tasks containing the given label
      * @param label - string with label
      * @param sort_by - enum which informs by what parameter sort requested tasks
-     * @return ManyTasksWithId - protobuf message containing requesting tasks
+     * @return \b ManyTasksWithId - protobuf message containing requesting tasks
      */
     virtual ManyTasksWithId ShowByLabel(const std::string& label, const TasksSortBy& sort_by);
     /**
      * Shows all tasks expect subtasks
      * @param sort_by - enum which informs by what parameter sort requested tasks
-     * @return ManyTasksWithId - protobuf message containing requesting tasks
+     * @return \b ManyTasksWithId - protobuf message containing requesting tasks
      */
     virtual ManyTasksWithId ShowParents(const TasksSortBy& sort_by);
     /**
      * Shows task with given id with ith subtasks
      * @param id - protobuf message containing id of task to be shown
      * @param sort_by - enum which informs by what parameter sort children of requesting task
-     * @return CompositeTask - protobuf message containing requesting task
+     * @return \b CompositeTask - protobuf message containing requesting task
      */
     virtual CompositeTask ShowTask(const TaskId& id, const TasksSortBy& sort_by);
     /**
      * Shows all tasks with subtasks
      * @param sort_by - enum which informs by what parameter sort requested tasks
-     * @return ManyCompositeTasks - protobuf message containing requesting tasks
+     * @return \b ManyCompositeTasks - protobuf message containing requesting tasks
      */
     virtual ManyCompositeTasks ShowAll(const TasksSortBy& sort_by);
 
     /**
-     * Saves state of TaskManager to given file
+     * Saves state of \b TaskManager to given file
      * @param filename - string with name of file
-     * @return bool - is saving successful
+     * @return \b bool - is saving successful
      */
     virtual bool Save(const std::string& filename);
     /**
-     * Loads state of TaskManager from existing file
+     * Loads state of \b TaskManager from existing file
      * @param filename - string with name of file
-     * @return bool - is loading successful
-     * @warning Current state of TaskManager will be completely erased
+     * @return \b bool - is loading successful
+     * @warning Current state of \b TaskManager will be completely erased
      */
     virtual bool Load(const std::string& filename);
 private:
--- src/ui/controller/Controller.h
+++ src/ui/controller/Controller.h
@@ -3,12 +3,12 @@
 #include "Task.pb.h"
 #include "TaskStructures.pb.h"
 
+namespace ui {
 /**
  * \brief Model controller interface
- * \details Passes requests to model
+ * \details Passes requests to \b Model
  * \author Michael Kupriianov
  */
-namespace ui {
 class Controller {
 public:
     /**
@@ -19,33 +19,33 @@ public:
     /**
      * Sends request to adds a task
      * @param task - protobuf message containing the task to be added
-     * @return bool - is addition successful
+     * @return \b bool - is addition successful
      */
     virtual bool AddTask(const Task& task) = 0;
     /**
      * Sends request to adds a subtask with given id of parent
      * @param task - protobuf message containing the subtask to be added
      * @param id - protobuf message containing the id of parent
-     * @return bool - is addition successful
+     * @return \b bool - is addition successful
      */
     virtual bool AddSubTask(const Task& task, const TaskId& id) = 0;
     /**
      * Sends request to edits a task
      * @param id - protobuf message containing id of task to be edited
      * @param task - protobuf message containing new content of task
-     * @return bool - is edition successful
+     * @return \b bool - is edition successful
      */
     virtual bool Edit(const TaskId& id, const Task& task) = 0;
     /**
      * Sends request to completes task with given id
      * @param id - protobuf message containing id of task to be completed
-     * @return bool - is completion successful
+     * @return \b bool - is completion successful
      */
     virtual bool Complete(const TaskId& id) = 0;
     /**
      * Sends request to deletes task with given id
      * @param id - protobuf message containing id of task to be deleted
-     * @return bool - is deletion successful
+     * @return \b bool - is deletion successful
      */
     virtual bool Delete(const TaskId& id) = 0;
 
@@ -53,39 +53,39 @@ public:
      * Sends request to shows all tasks containing the given label
      * @param label - string with label
      * @param sort_by - enum which informs by what parameter sort requested tasks
-     * @return ManyTasksWithId - protobuf message containing requesting tasks
+     * @return \b ManyTasksWithId - protobuf message containing requesting tasks
      */
     virtual ManyTasksWithId ShowByLabel(const std::string& label, const TasksSortBy& sort_by) = 0;
     /**
      * Sends request to shows all tasks expect subtasks
      * @param sort_by - enum which informs by what parameter sort requested tasks
-     * @return ManyTasksWithId - protobuf message containing requesting tasks
+     * @return \b ManyTasksWithId - protobuf message containing requesting tasks
      */
     virtual ManyTasksWithId ShowParents(const TasksSortBy& sort_by) = 0;
     /**
      * Sends request to shows task with given id with ith subtasks
      * @param id - protobuf message containing id of task to be shown
      * @param sort_by - enum which informs by what parameter sort children of requesting task
-     * @return CompositeTask - protobuf message containing requesting task
+     * @return \b CompositeTask - protobuf message containing requesting task
      */
     virtual CompositeTask ShowTask(const TaskId& id, const TasksSortBy& sort_by) = 0;
     /**
      * Sends request to shows all tasks with subtasks
      * @param sort_by - enum which informs by what parameter sort requested tasks
-     * @return ManyCompositeTasks - protobuf message containing requesting tasks
+     * @return \b ManyCompositeTasks - protobuf message containing requesting tasks
      */
     virtual ManyCompositeTasks ShowAll(const TasksSortBy& sort_by) = 0;
 
     /**
      * Sends request to saves state of model to given file
      * @param filename - string with name of file
-     * @return bool - is saving successful
+     * @return \b bool - is saving successful
      */
     virtual bool Save(const std::string& filename) = 0;
     /**
      * Sends request to loads state of model from existing file
      * @param filename - string with name of file
-     * @return bool - is loading successful
+     * @return \b bool - is loading successful
      */
     virtual bool Load(const std::string& filename) = 0;
 };
