8094e6afbd0a5089c1971b90161861af3d6568e0
mykhailo.kupiianov <mykhailo.kupriianov@teamdev.com>
2022-02-22 18:16:14 +0200

    Added documentation for ui::Controller

--- src/ui/controller/Controller.h
+++ src/ui/controller/Controller.h
@@ -3,23 +3,90 @@
 #include "Task.pb.h"
 #include "TaskStructures.pb.h"
 
+/**
+ * \brief Model controller interface
+ * \details Passes requests to model
+ * \author Michael Kupriianov
+ */
 namespace ui {
 class Controller {
 public:
+    /**
+     * Virtual destructor for correct inheritance
+     */
     virtual ~Controller() = default;
 
+    /**
+     * Sends request to adds a task
+     * @param task - protobuf message containing the task to be added
+     * @return bool - is addition successful
+     */
     virtual bool AddTask(const Task& task) = 0;
+    /**
+     * Sends request to adds a subtask with given id of parent
+     * @param task - protobuf message containing the subtask to be added
+     * @param id - protobuf message containing the id of parent
+     * @return bool - is addition successful
+     */
     virtual bool AddSubTask(const Task& task, const TaskId& id) = 0;
+    /**
+     * Sends request to edits a task
+     * @param id - protobuf message containing id of task to be edited
+     * @param task - protobuf message containing new content of task
+     * @return bool - is edition successful
+     */
     virtual bool Edit(const TaskId& id, const Task& task) = 0;
+    /**
+     * Sends request to completes task with given id
+     * @param id - protobuf message containing id of task to be completed
+     * @return bool - is completion successful
+     */
     virtual bool Complete(const TaskId& id) = 0;
+    /**
+     * Sends request to deletes task with given id
+     * @param id - protobuf message containing id of task to be deleted
+     * @return bool - is deletion successful
+     */
     virtual bool Delete(const TaskId& id) = 0;
 
-    virtual ManyTasksWithId ShowByLabel(const std::string& label, const TasksSortBy&) = 0;
-    virtual ManyTasksWithId ShowParents(const TasksSortBy&) = 0;
-    virtual CompositeTask ShowTask(const TaskId& id, const TasksSortBy&) = 0;
-    virtual ManyCompositeTasks ShowAll(const TasksSortBy&) = 0;
+    /**
+     * Sends request to shows all tasks containing the given label
+     * @param label - string with label
+     * @param sort_by - enum which informs by what parameter sort requested tasks
+     * @return ManyTasksWithId - protobuf message containing requesting tasks
+     */
+    virtual ManyTasksWithId ShowByLabel(const std::string& label, const TasksSortBy& sort_by) = 0;
+    /**
+     * Sends request to shows all tasks expect subtasks
+     * @param sort_by - enum which informs by what parameter sort requested tasks
+     * @return ManyTasksWithId - protobuf message containing requesting tasks
+     */
+    virtual ManyTasksWithId ShowParents(const TasksSortBy& sort_by) = 0;
+    /**
+     * Sends request to shows task with given id with ith subtasks
+     * @param id - protobuf message containing id of task to be shown
+     * @param sort_by - enum which informs by what parameter sort children of requesting task
+     * @return CompositeTask - protobuf message containing requesting task
+     */
+    virtual CompositeTask ShowTask(const TaskId& id, const TasksSortBy& sort_by) = 0;
+    /**
+     * Sends request to shows all tasks with subtasks
+     * @param sort_by - enum which informs by what parameter sort requested tasks
+     * @return ManyCompositeTasks - protobuf message containing requesting tasks
+     */
+    virtual ManyCompositeTasks ShowAll(const TasksSortBy& sort_by) = 0;
 
+    /**
+     * Sends request to saves state of model to given file
+     * @param filename - string with name of file
+     * @return bool - is saving successful
+     */
     virtual bool Save(const std::string& filename) = 0;
+    /**
+     * Sends request to loads state of model from existing file
+     * @param filename - string with name of file
+     * @return bool - is loading successful
+     */
     virtual bool Load(const std::string& filename) = 0;
 };
 }
